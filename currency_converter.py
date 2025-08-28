21d674f8ad21dfb37a2ad211478dba69874893fc
Haaziq Abbasi <haaziq24@gmail.com>
2025-08-28 01:20:14 -0400

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -8,8 +8,9 @@ reais_exch = reais/5.45
 
 total_usd = pesos_exch + soles_exch + reais_exch
 
-print(f'Remaining balance from Pesos $ {pesos_exch:.2f}')
-print(f'Remaining balance from Soles $ {soles_exch:.2f}')
-print(f'Remaining balance from Reais $ {reais_exch:.2f}')
+print(f'Remaining balance from Pesos: $ {pesos_exch:.2f}')
+print(f'Remaining balance from Soles: $ {soles_exch:.2f}')
+print(f'Remaining balance from Reais: $ {reais_exch:.2f}')
+
 
 print(f'Total in USD: $ {total_usd:.2f}')
